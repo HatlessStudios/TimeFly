0b16d2fc4b98d3a9ed8bcf7b02e37c37ac20ae1b
= <chris.phippen98@gmail.com>
2018-02-10 23:59:58 +0000

    Remake basic server model.

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
-const http = require("http");
-const fs = require("fs");
+var express = require("express");
+var app = express();
+var router = express.Router();
 
-const port = 3000;
-const hostname = "127.0.0.1";
-
-fs.readFile("index.html", function (err, html) {
-	if(err){
-		throw err;
-	}
-
-	const server = http.createServer(function (req, res) {
-		res.statusCode = 200;
-		res.setHeader("Content-type", "text/html");
-		res.write(html);
-		res.end();
-	});
-
-	
-
+router.get('/', function(req, res){
+	res.sendFile("index.html");
 });
+
+app.listen(3000, function (){
+	console.log("Live at port " + 3000);
+})
